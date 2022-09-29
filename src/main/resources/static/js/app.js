3eeafe52666309454893e7bcf99e26c75a2b7827
Cristian Ruiz <cristiansanta13@hotmail.com>
2022-09-28 21:33:44 -0500

    Punto 5, 6 y 7

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,20 +1,43 @@
 var modulo = (function(){
-    var authorBluePrint;
-    var bluePrints = [];
-    //var moduloApimock = apimock;
+    var _author;
+    var _bluePrints = [];
+    var _modulo = apimock;
 
     var getBluePrintsByAuthor = function(){
-        bluePrints = [];
+        _bluePrints = [];
         $("#idTableBluePrints > tbody").empty(); //elimina todos los nodos secundarios y el contenido de los elementos seleccionados.
-        authorBluePrint = document.getElementById("inputAuthor").value;
-        setAuthorBluePrint(authorBluePrint);
+        _author = document.getElementById("inputAuthor").value;
+        setAuthorBluePrint(_author);
+        setBlueprints(_author);
     }
 
     var setAuthorBluePrint = function(nameBluePrint){
         $("#idNameBluePrintTitulo").text(nameBluePrint + "´s blueprints:")
     }
 
+    var setBlueprints = function(author){
+        _modulo.getBlueprintsByAuthor(author, _getNameAndSize)
+    }
+
+    var _getNameAndSize = function(blueprintsArray){
+        _blueprints = blueprintsArray.map(blueprint => [blueprint.name, blueprint.points.length]);
+        _setTable(_blueprints);
+    }
+
+    var _setTable = function(blueprintsArray){
+        blueprintsArray.map(blueprint => $("table tbody").append("<tr><td>"+ (blueprintsArray.indexOf(blueprint)+1) +"</td><td>" + blueprint[0] + "</td><td>" + blueprint[1] + "</td><td><button type='button' class='btn btn-outline-success' id="+blueprint[0] +" "+ "type='button' onclick=module.getBluePrintToShow(this)>Open</button></td></tr>"));
+                var numArray = blueprintsArray.map(blueprint => blueprint[1]);
+        $("#totalPoints").text(" Total user points: " + numArray.reduce((previousValue, currentValue) => previousValue + currentValue, 0));
+    }
+
+    var getBluePrintToShow = function(button){
+        /*idBlueprint = button.id;
+        console.log("ID DEL PLANO " + idBlueprint)
+        _module.getBlueprintsByNameAndAuthor(_author,idBlueprint,_drawInCanvas);*/
+    }
+
     return{
-        getBluePrintsByAuthor:getBluePrintsByAuthor
+        getBluePrintsByAuthor:getBluePrintsByAuthor,
+        getBluePrintToShow:getBluePrintToShow
     };
 })();
